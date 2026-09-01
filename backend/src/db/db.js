d1ba5307fd712a06b7bcc9fbb5c2ae351396863c
Wahaj Khan Ahmed <ahmedwahaj317@gmail.com>
2026-09-01 16:48:38 -0700

    Refactor MongoDB connection handling
    Refactor connectDB function to use a connection promise for MongoDB connection management.

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,16 +1,43 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
 
-function connectDB(){
-    mongoose.connect(process.env.MONGODB_URI)
-    .then(()=>{
-   console.log("MongoDB is connected")
-   console.log("Everything is clear sir.")
-    })
-    .catch((error)=>{
-        console.log("Mongodb connection error is:",error)
-        
-    })
+let connectionPromise = null
+
+
+function connectDB() {
+
+    if (!connectionPromise) {
+
+        connectionPromise =
+            mongoose.connect(
+                process.env.MONGODB_URI
+            )
+            .then(() => {
+
+                console.log('MongoDB is connected')
+                console.log('Everything is clear sir.')
+
+                return mongoose.connection
+
+            })
+            .catch((error) => {
+
+                console.log(
+                    'Mongodb connection error is:',
+                    error
+                )
+
+                connectionPromise = null
+
+                throw error
+
+            })
+
+    }
+
+    return connectionPromise
+
 }
+
 
 module.exports = connectDB
