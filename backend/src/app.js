4945766a632ce14eb4dfe117beab473586f49ac1
Wahaj Khan Ahmed <ahmedwahaj317@gmail.com>
2026-09-02 04:28:43 -0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,23 @@ const cors = require('cors')
 
 
 const app = express()
+
+
+
+app.set('etag', false)
+
+app.use((req, res, next) => {
+
+    res.set(
+        'Cache-Control',
+        'no-store'
+    )
+
+    next()
+
+})
+
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials:true
@@ -14,6 +31,13 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json())
 
+
+
+app.use("/api/auth",authRoutes)
+app.use("/api/food",foodRoutes)
+app.use("/api/foodpartner",foodPartnerRoutes)
+
+module.exports = app;
 
 
 app.use("/api/auth",authRoutes)
